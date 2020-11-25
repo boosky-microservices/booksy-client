053bca884913761263891973ab107aa946d48225
YassineCherifi <mohyassine.cherifi@gmail.com>
2020-11-25 15:42:02 +0100

    update environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
     CLIENT_ID: 'I67fLdffMBUg0VceI5Uv7nDnrxosWXAT',
     DOMAIN: 'dev-ohvjdegt.eu.auth0.com',
     AUDIENCE: 'https://booksy-api.herokuapp.com/',
-    BASE_URL: 'https://booksy-api.herokuapp.com',
+    BASE_URL: 'http://booksy-angular-client.s3-website.eu-west-3.amazonaws.com',
     SCOPE: 'openid profile email'
   },
   API_URL: 'https://33gssbff04.execute-api.eu-west-3.amazonaws.com/dev'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
     CLIENT_ID: 'I67fLdffMBUg0VceI5Uv7nDnrxosWXAT',
     DOMAIN: 'dev-ohvjdegt.eu.auth0.com', // e.g., https://you.auth0.com/
     AUDIENCE: 'https://booksy-api.herokuapp.com/', // e.g., http://localhost:3001
-    BASE_URL: 'http://localhost:4200',
+    BASE_URL: 'http://booksy-angular-client.s3-website.eu-west-3.amazonaws.com',
     SCOPE: 'openid profile email'
   },
   API_URL: 'https://33gssbff04.execute-api.eu-west-3.amazonaws.com/dev'
